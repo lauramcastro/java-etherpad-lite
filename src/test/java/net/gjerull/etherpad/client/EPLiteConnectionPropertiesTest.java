6868229dbfad1bbcafc58f167283507073f1e9e1
Laura M. Castro <lcastro@udc.es>
2018-12-01 00:33:27 +0100

    Add configuration to tune down verbosity of JUnit-QuickCheck.

--- src/test/java/net/gjerull/etherpad/client/EPLiteConnectionPropertiesTest.java
+++ src/test/java/net/gjerull/etherpad/client/EPLiteConnectionPropertiesTest.java
@@ -3,8 +3,8 @@ package net.gjerull.etherpad.client;
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
 import com.pholser.junit.quickcheck.Property;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
 
 @RunWith(JUnitQuickcheck.class)
@@ -13,6 +13,13 @@ public class EPLiteConnectionPropertiesTest {
     private static final String API_VERSION = "1.2.12";
     private static final String ENCODING = "UTF-8";
 
+    @Before
+    public void setUp() {
+        ((ch.qos.logback.classic.Logger) org.slf4j.LoggerFactory
+         .getLogger("junit-quickcheck.value-reporting"))
+            .setLevel(ch.qos.logback.classic.Level.OFF);
+    }
+    
     @Property
     public void domain_with_trailing_slash_when_construction_an_api_path(String exampleMethod) throws Exception {
 
